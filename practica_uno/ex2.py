1e42d1bf2466cd48a6aedad04d53e126e89b9e22
EC2 Default User <ec2-user@ip-172-31-35-78.us-east-2.compute.internal>
2022-09-28 23:38:01 +0000

    ex2

--- practica_uno/ex2.py
+++ practica_uno/ex2.py
@@ -1,17 +1,29 @@
-#este codigo es para identifacr pares 
-#declarama la primera función 
+#este codigo es para identificar pares 
 import sys 
 
 x= int(sys.argv[1])
+#declaramos la primera función 
 def funcion(x): 
-    st=[]
-    for i in range(1, x+1):
-        st.append(i)
+    st=[] #Declaramos la lista 
+    for i in range(1, x+1): # hacemos un ciclo para desglozar el numero dado 
+        st.append(i) #ingresamos los valores a la lista 
     return(st)
 st=funcion(x)
 print(st)
     
-
+def primos(x): 
+    cadena=[]
+    for i in range(1, x+1): #contador para 
+        p = 0
+        for j in range(1, i+1):
+            if i % j == 0:
+                p += 1
+        if p == 2:
+            cadena.append(i)
+    return(cadena)
+cadena=primos(x)
+print("Primos:", cadena)
+    
     #ingresamos el dato 
         
  
